0d15df06a7a68867f5938161cded490022170e09
Jason Gauci <jgmath2000@gmail.com>
2024-08-28 13:17:45 -0500

    fix typo

--- frontend/app/src/components/utils/API/AptGptUtility.js
+++ frontend/app/src/components/utils/API/AptGptUtility.js
@@ -39,6 +39,7 @@ class AptGptUtility {
     const res = await this.post("datas/modelTwo", {
       'load_model': true,
     });
+  }
 
   async datas_fetch_apt(apt_id) {
     const res = await globalRequestManager.enqueueRequest(() =>
