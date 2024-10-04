1ef3523d6ddd0d2920cfe813f6016bbfffca2226
Matthew Gunton <mgunton7@gmail.com>
2024-10-04 12:54:01 -0500

    better logging on backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
 import { errorResponder } from "./middleware/errorResponder";
+const { v4: uuidv4 } = require('uuid');
+const morgan = require('morgan');
 
 export const app = express();
 
@@ -9,6 +11,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(apiRoutes);
 app.use(errorResponder);
+
+// Middleware to add request ID to each request
+app.use((req: any, res, next) => {
+    const requestId = uuidv4();
+    req.requestId = requestId;  // Attach the request ID to the request object
+    res.setHeader('X-Request-Id', requestId); // Optionally add it to the response headers
+
+    const originalConsoleLog = console.log;
+    console.log = (...args) => {
+        originalConsoleLog(`[Request ID: ${requestId}]`, ...args);
+    };
+
+    const originalConsoleError = console.error;
+    console.error = (...args) => {
+        originalConsoleError(`[Request ID: ${requestId}]`, ...args);
+    };
+
+    next();
+});
+
+// Morgan token to log request ID
+morgan.token('id', function getId(req: any) {
+    return req.requestId;
+});
+
+const loggerFormat = '[:date[clf]] :id :method :url :status :response-time ms - :res[content-length]';
+app.use(morgan(loggerFormat));
 
 //
 import blog from "./api/blog";
